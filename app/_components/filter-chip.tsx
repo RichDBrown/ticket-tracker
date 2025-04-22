ba5186ff90bb786dca79422039ff260562e9da2d
RichDBrown <richarddb602@gmail.com>
2025-04-22 16:29:08 -0500

    Add checkmark, elevation, and make text smaller for filter-chip component

--- app/_components/filter-chip.tsx
+++ app/_components/filter-chip.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Image from "next/image"
+import check from "@/public/check.svg"
 import { Dispatch, SetStateAction } from "react"
 
 type FilterChipProps = {
@@ -19,6 +21,9 @@ export default function FilterChip({ name, isSelected, setIsSelected }: FilterCh
   }
 
   return (
-    <button onClick={setSelected} className={`rounded-sm px-1 h-2.5 flex items-center text-ll font-med cursor-pointer ${isSelected === name ? 'text-on-primary-container bg-primary-container' : 'border border-outline-variant hover:bg-[#DFDDE4] active:bg-[#D2D0D7]'}`}>{name}</button>
+    <button onClick={setSelected} className={`rounded-sm h-2.5 flex items-center text-lm font-med cursor-pointer transition-all ${isSelected === name ? 'pl-0.5 pr-1 text-on-primary-container bg-primary-container hover:shadow-elevation-1' : 'px-1 border border-outline-variant hover:bg-[#DFDDE4] active:bg-[#D2D0D7]'}`}>
+      {isSelected === name && <Image src={check} alt="Selected" className="mr-0.5" />}
+      <p>{name}</p>
+    </button>
   )
 }
